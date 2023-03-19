ba68e81c8dd11a5d06f2743a7d7408c52f92779d
GooshGoosh <113269398+GooshGoosh@users.noreply.github.com>
2023-03-19 06:43:38 -0500

    Add new files to repository
    Added programs collatzSequence.py and zigzag.py

---
+++ collatzSequence.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+def collatz(number):
+    if number % 2 == 0:
+        print(number // 2)
+        return (number // 2)
+    elif number % 2 == 1:
+        print((3 * number) + 1)
+        return ((3 * number) + 1)
+
+
+def main():
+    try:
+        num = int(input('\nPlease enter a number: '))
+        while num != 1:
+            num = collatz(num)
+    except ValueError:
+        print('\nEnter a number!')
+
+
+if __name__ == "__main__":
+    main()
+
