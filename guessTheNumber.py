95892ad9894b491993ab47a06ca0428df5ef2cb4
GooshGoosh <113269398+GooshGoosh@users.noreply.github.com>
2023-03-18 21:40:01 -0500

    Add files to repository
    Added the files guessTheNumber.py and rpsGame.py

---
+++ guessTheNumber.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# This is a guess the number game.
+
+import random
+import pyinputplus as pyip
+
+def main():
+    secretNumber = random.randint(1, 20)
+    print('\nI am thinking of a number between 1 and 20.')
+
+    # Ask the player to guess 6 times.
+    for guessesTaken in range(1, 7):
+        print('Take a guess.')
+        guess = pyip.inputNum(min=1, max=20)
+
+        if guess < secretNumber:
+            print('Your guess is too low.')
+        elif guess > secretNumber:
+            print('Your guess is too high.')
+        else:
+            break       # This condition is the correct guess.
+
+    if guess == secretNumber:
+        print('Good job! You guessed my number in {} guesses!'.format(guessesTaken))
+    else:
+        print('Nope. The number I was thinking of was {}'.format(secretNumber))
+
+
+if __name__ == "__main__":
+    main()
+
