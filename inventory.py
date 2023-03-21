19e45a8bb7ea0cbf973cd879826edd7b60929391
GooshGoosh <113269398+GooshGoosh@users.noreply.github.com>
2023-03-21 17:51:52 -0500

    Add function addToInventory

--- inventory.py
+++ inventory.py
@@ -9,9 +9,24 @@ def displayInventory(inventory):
     print("\nTotal number of items: {}".format(itemTotal))
 
 
+def addToInventory(inventory, addedItems):
+    print('\nUpdating inventory...')
+    for item in addedItems:
+        if item in inventory.keys():
+            inventory[item] += 1
+        else:
+            inventory[item] = 1
+
+    return inventory
+
+
 def main():
     stuff = {'Rope': 1, 'Torch': 6, 'Gold Coin': 42, 'Dagger': 1, 'Arrow': 12}
+    dragonLoot = ['Gold Coin', 'Dagger', 'Gold Coin', 'Ruby']
 
+    displayInventory(stuff)
+    
+    stuff = addToInventory(stuff, dragonLoot)
     displayInventory(stuff)
 
 
