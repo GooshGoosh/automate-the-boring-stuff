d4218057dde7ebbe64f3570ebdca0f678c3a0d2a
GooshGoosh <113269398+GooshGoosh@users.noreply.github.com>
2023-04-29 18:42:03 -0500

    Finish program functionality
    Added the ability for the program to insert a number of blank
    rows at a specific starting row, both values given by the user.
    Also added the functionality for the program to save the newly
    created spreadsheet file in the same directory path as the file
    given by the user.
    
    Added a try/except clause to ensure that the first two arguments
    given to the program, the starting row and the number of rows to
    add, can properly be converted into integer types.

--- blankRowInserter.py
+++ blankRowInserter.py
@@ -28,17 +28,36 @@ def insert_blank_rows(N, M, spreadsheetFile):
         print('\nFile {} is not a valid spreadsheet file!\nExiting...'.format(spreadsheet.name))
         sys.exit(1)
 
+    print('\nLoading workbook...')
+    wb = openpyxl.load_workbook(spreadsheet)
+    sheet = wb.active
+    print('Inserting {} blank rows at row {}...'.format(M, N))
+    sheet.insert_rows(N, amount=M)  # Insert M blank rows starting at row N.
+
+    parentPath = spreadsheet.parent                 # Set the parent path.
+    filename = str(spreadsheet.stem)                # Get the basename of the spreadsheet.
+    newFilename = filename + '-inserted-rows.xlsx'  # Create new filename for spreadsheet.
+    wb.save(parentPath / newFilename)
+
+    print('Done!')
+    print('File saved as {} in {}'.format(newFilename, parentPath))
+
 
 def main():
+    # Ensure the correct number of command-line arguments.
     if len(sys.argv) != 4:
         print('\nUsage: blankRowInserter.py <integer> <integer> <path/to/spreadsheet')
         sys.exit(1)
 
-    startingRow = sys.argv[1]
-    rowsToAdd = sys.argv[2]
-    spreadsheet = sys.argv[3]
-
-    insert_blank_rows(startingRow, rowsToAdd, spreadsheet)
+    try:
+        startingRow = int(sys.argv[1])
+        rowsToAdd = int(sys.argv[2])
+        spreadsheet = sys.argv[3]
+        insert_blank_rows(startingRow, rowsToAdd, spreadsheet)
+    except ValueError:
+        print('\nPlease enter a whole number (integer) for the first two arguments...')
+        print('Exiting...')
+        sys.exit(1)
 
 
 if __name__ == "__main__":
